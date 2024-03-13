c5b1dbc6e4e096b83aae457e55cacad58c4113f9
NME <adimaharshi20@gmail.com>
2024-03-14 04:35:26 +0530

    extra

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -2,6 +2,13 @@ print("HELLO AND WELCOME TO BAND NAME GENERATOR:")
 pet_name = input("WHAT IS THE NAME OF YOUR PET:\n")
 city = input("WHICH CITY YOU GROW UP IN?:\n")
 
-band_name = '"' + pet_name + " " +  city + '"'
+adjectives = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard","aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","abusive","acceptable","accessible","accidental","accurate","acid","acidic","acoustic","acrid","actually","ad","hoc","adamant","adaptable"]
+
+nouns = ["accelerator", "accordion", "account", "accountant", "acknowledgment", "acoustic", "acrylic", "act", "action", "active", "activity", "actor", "actress", "adapter", "addition", "address", "adjustment", "adult", "advantage", "advertisement", "advice", "afghanistan", "africa", "aftermath", "afternoon", "aftershave", "afterthought", "age", "agenda", "agreement", "air", "airbus", "airmail", "airplane", "airport", "airship", "alarm", "albatross", "alcohol", "algebra", "algeria", "alibi", "alley", "alligator", "alloy", "almanac", "alphabet", "alto", "aluminium", "aluminum", "ambulance", "america", "amount", "amusement", "anatomy"]
+
+verbs = ["accept", "add", "admire", "admit", "advise", "afford", "agree", "alert", "allow", "amuse", "analyse", "announce", "annoy", "answer", "apologise", "appear", "applaud", "appreciate", "approve", "argue", "arrange", "arrest", "arrive", "ask", "attach", "attack", "attempt", "attend", "attract", "avoid", "back", "bake", "balance", "ban", "bang", "bare", "bat", "bathe", "battle", "beam", "beg", "behave", "belong", "bleach", "bless", "blind", "blink", "blot", "blush", "boast", "boil", "bolt", "bomb", "book", "bore", "borrow", "bounce", "bow", "box", "brake", "branch", "breathe", "bruise", "brush", "bubble", "bump", "burn", "bury", "buzz", "calculate", "call", "camp"]
+
+import random
+band_name = '"'+ random.choice(adjectives) + pet_name.upper() +" " + random.choice(verbs)+ city.upper() + '"'
 print("HURRAY YOU GOT YOUR BAND NAME, " + band_name + " ,THAT'S DAMN COOL!!") 
 
